8af1ba1e03b554c4e8c392b2b56fff3762067a40
Soham Bhosale <sohambhosle26@gmail.com>
2023-02-01 14:07:36 +0530

    upgraded the file-saver url to https

--- client/src/utils/index.js
+++ client/src/utils/index.js
@@ -11,5 +11,6 @@ export function getRandomPrompt(prompt) {
 }
 
 export async function downloadImage(_id, photo) {
-    FileSaver.saveAs(photo, `download-${_id}.jpg`);
+    const url = photo.substring(0, 4) + "s" + photo.substring(4, photo.length);
+    FileSaver.saveAs(url, `download-${_id}.jpg`);
 }
